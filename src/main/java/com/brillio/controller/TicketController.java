485b037669fd7e1ffa07e8d52d412817c1d7f08c
FareedKhanGithub <fareedkhan2552@gmail.com>
2021-07-03 10:43:53 -0500

    Base weekend

--- src/main/java/com/brillio/controller/TicketController.java
+++ src/main/java/com/brillio/controller/TicketController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.brillio.entity.Ticket;
 import com.brillio.service.TicketService;
@@ -28,7 +31,46 @@ public class TicketController {
 		
 		List<Ticket> theTicket = ticketService.findAll();
 		theModel.addAttribute("tickets", theTicket);
-		return "list-tickets";
+		return "tickets/list-tickets";
+	}
+	
+	@GetMapping("/showFormForBuy")
+	public String showFormForBuy(Model theModel) {
+		Ticket theTicket = new Ticket();
+		
+		theModel.addAttribute("ticket", theTicket);
+		
+		return "tickets/buy-form";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("ticketId") int tikcetId, Model theModel) {
+		
+		System.out.println("**********");
+		
+		//get the ticket from the service
+		Ticket theTicket = ticketService.findById(tikcetId);
+		//set ticket as a model attribute
+		
+		theModel.addAttribute("ticket", theTicket);
+		//send over to the form
+		
+		System.out.println("*********" + theTicket.getId());
+		return "tickets/buy-form";
+	}
+	
+	@PostMapping("/buy")
+	public String saveTicket(@ModelAttribute("ticket") Ticket theTicket) {
+		//save the employee
+		ticketService.save(theTicket);
+		//use a redirect to prevent duplicate submissions 
+		return "redirect:/tickets/list";
+	}
+	
+	@GetMapping("/delete") 
+	public String delete(@RequestParam("ticketId") int theId) {
+		ticketService.deleteById(theId);
+		return "redirect:/tickets/list";
 	}
 	
 }
